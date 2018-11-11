c02775133495a2b58ef0b5d12e3cd9c75cd0e801
YuriU <jerryhire8@gmail.com>
2018-11-11 11:05:50 +0200

    Added options example

---
+++ 01_Options/Options.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace _01_Options
+{
+    // type Option t = None | Some t
+    public struct Option<T>
+    {
+        private readonly bool isSome;
+        private readonly T value;
+
+        private Option(T value)
+        {
+            this.isSome = true;
+            this.value = value;
+        }
+
+        public static implicit operator Option<T>(Options.None _)
+            => new Option<T>();
+
+        public static implicit operator Option<T>(Options.Some<T> some)
+            => new Option<T>(some.Value);
+
+        public R Match<R>(Func<R> None, Func<T, R> Some) 
+            => isSome ? Some(value) : None();
+    }
+
+    public static partial class F
+    {
+        public static Options.None None => Options.None.Default;
+
+        public static Options.Some<T> Some<T>(T value) => new Options.Some<T>(value);
+    }
+
+    namespace Options
+    {
+        public struct None
+        {
+            internal static readonly None Default = new None();
+        }
+
+        public struct Some<T>
+        {
+            internal T Value { get; }
+
+            internal Some(T value)
+            {
+                if(value == null)
+                    throw new ArgumentNullException();
+
+                Value = value;
+            }           
+        }   
+    }
+}
---
+++ 01_Options/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static _01_Options.F;
+
+namespace _01_Options
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Option<string> firstName = Some("John");
+
+            Option<string> lastName = None;
+
+            Print(firstName);
+
+            Print(lastName);
+
+            Console.Read();
+        }
+
+        public static void Print(Option<string> value)
+        {
+            var nameToPrint = value.Match(
+                () => "No value provided", 
+                s => s);
+
+            Console.WriteLine(nameToPrint);
+        }
+    }
+}
