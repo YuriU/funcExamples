b7fa929b0aa8c6bcaee45946df422d4c764e2de2
YuriU <jerryhire8@gmail.com>
2018-11-11 13:04:14 +0200

    Added error handling example

---
+++ 04_ErrorHandling/FunctionalExtensions.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static _04_ErrorHandling.F;
+
+namespace _04_ErrorHandling
+{
+    public static class FunctionalExtensions
+    {
+        public static Option<R> Map<T, R>(this Option<T> optT, Func<T, R> f) 
+            => optT.Match(
+                () => (Option<R>)None, 
+                (t) => Some(f(t)));
+
+        public static Option<R> Bind<T, R>(this Option<T> optT, Func<T, Option<R>> f) 
+            => optT.Match(
+                () => (Option<R>)None, 
+                (t) => f(t));
+
+        public static Either<L, Res> Bind<L, R, Res>(this Either<L, R> eithLR, Func<R, Either<L, Res>> f) 
+            => eithLR.Match(
+                l => Left(l), 
+                r => f(r));
+    }
+}
---
+++ 04_ErrorHandling/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using static _04_ErrorHandling.F;
+
+namespace _04_ErrorHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Print example
+            PrintExample();
+
+            // 2. Parse example
+            MainLoop();
+        }
+
+        private static void PrintExample()
+        {
+            Either<string, int> numRes = Right(3);
+
+            Either<string, string> error = Left("Unable to parse string");
+
+            Print(numRes);
+
+            Print(error);
+        }
+
+        public static void Print<L, R>(Either<L, R> either)
+        {
+            var nameToPrint = either.Match(
+                left => left.ToString(), 
+                right => right.ToString());
+
+            Console.WriteLine(nameToPrint);
+        }
+
+        private static string ProcessInput(Either<string, string> input)
+        {
+            var res = input
+            .Bind(ParseStringToInt)
+            .Bind(GetSqrt)
+            .Match(
+                l => l,
+                r => r.ToString());
+                
+            return res;
+        }
+
+        public static Either<string, int> ParseStringToInt(string line)
+        {
+            if (int.TryParse(line, out var number))
+            {
+                return Right(number);
+            }
+            else
+            {
+                return Left("The string you entered is not a number");
+            }
+        }
+
+        public static Either<string, double> GetSqrt(int number)
+        {
+            if(number < 0)
+            {
+                return Left("The number is above zero");
+            }
+            else
+            {
+                return Right(Math.Sqrt(number));
+            }
+        }
+
+        private static void MainLoop()
+        {
+            do
+            {
+                Console.WriteLine("Enter some value:");
+                var line = Console.ReadLine();
+
+                Either<string, string> input = !string.IsNullOrWhiteSpace(line) 
+                            ? (Either<string, string>)Right(line) 
+                            : Left("The string is empty");
+
+                var output = ProcessInput(input);
+
+                Console.WriteLine(output);
+
+            } 
+            while (true);
+        }
+    }
+}
