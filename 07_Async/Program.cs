fa6f458596efe2adb39af62f72dae7fb365f5f4b
YuriU <jerryhire8@gmail.com>
2018-11-12 00:34:15 +0200

    Make more examples

--- 07_Async/Program.cs
+++ 07_Async/Program.cs
@@ -12,7 +12,7 @@ namespace _07_Async
         public static async Task MainAsync(string[] args)
         {
               var val = await GetString()
-                        .Bind(Parse)
+                        .Bind(ParseOrZero)
                         .Map(r => r * 3)
                         .Map(s => s.ToString());
 
@@ -20,17 +20,32 @@ namespace _07_Async
 
             var resFluent = await
                         from s in GetString()
-                        from n in Parse(s)
+                        from n in ParseOrZero(s)
                         from m3 in FunctionalExtensions.Async(n * 3)
                         select m3.ToString();
 
             Console.WriteLine("Fluent result: " + resFluent);
+
+
+            //var res = await 
+            //            from firstStr in ReadConsoleAsync("Enter first number:")
+            //            from secondStr in ReadConsoleAsync("Enter second number:")
+            //            //from firstInt in Parse(firstStr)
         }
 
         public static async Task<string> GetString()
         {
             await Task.Delay(1000);
             return "3";
+        }
+
+        public static async Task<int> ParseOrZero(string str)
+        {
+            await Task.Delay(1000);
+            
+            int val = 0;
+            int.TryParse(str, out val);
+            return val;
         }
 
         public static async Task<int> Parse(string str)
@@ -40,6 +55,22 @@ namespace _07_Async
             int val = 0;
             int.TryParse(str, out val);
             return val;
+        }
+
+        public static async Task<Result<string>> ReadConsoleAsync(string label)
+        {
+            Console.WriteLine(label);
+            await Task.Delay(1000);
+
+            var str = Console.ReadLine();
+            if(string.IsNullOrWhiteSpace(str))
+            {
+                return Error("Input value is empty");
+            }
+            else
+            {
+                return Value(str);
+            }
         }
     }
 }
