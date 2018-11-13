4a3e2515ad96c2eb03172eb25670183571790371
Yuriy Ulyanets <yuriy.ulyanets@namecheap.com>
2018-11-13 11:30:49 +0200

    Added real world example project

---
+++ 08_RealAppExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _08_RealAppExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
